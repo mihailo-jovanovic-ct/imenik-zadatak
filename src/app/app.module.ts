35cb5d613953177695c01e9af8cbc896d682f2d2
Mihailo Jovanovic <mjovanovic@carnegietechnologies.com>
2020-05-18 10:26:18 +0200

    Removed extra code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { LjudiComponent } from './ljudi/ljudi.component';
 import { LjudiListaComponent } from './ljudi/ljudi-lista/ljudi-lista.component';
 import { LjudiPodaciComponent } from './ljudi/ljudi-podaci/ljudi-podaci.component';
-import { HeaderComponent } from './header/header.component';
 import { LjudiJedanComponent } from './ljudi/ljudi-lista/ljudi-jedan/ljudi-jedan.component';
 import { LjudiStartComponent } from './ljudi/ljudi-start/ljudi-start.component';
 import { LjudiEditComponent } from './ljudi/ljudi-edit/ljudi-edit.component';
@@ -19,7 +18,6 @@ import { LjudiFilterPipe } from './ljudi/ljudi-filter.pipe';
     LjudiComponent,
     LjudiListaComponent,
     LjudiPodaciComponent,
-    HeaderComponent,
     LjudiJedanComponent,
     LjudiStartComponent,
     LjudiEditComponent,
